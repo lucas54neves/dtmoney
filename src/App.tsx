0a9c61493925c78d13c9840eeb3c3c5d7da25223
Lucas Neves <lucas54neves@gmail.com>
2022-07-20 19:27:30 -0300

    feat: add transaction provider

--- src/App.tsx
+++ src/App.tsx
@@ -4,30 +4,30 @@ import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from "./components/NewTransactionModal";
 import { GlobalStyle } from "./styles/global";
+import { TransactionProvider } from "./hooks/useTransactions";
 
 Modal.setAppElement("#root");
 
 export function App() {
-    const [isNewTransactionModalOpen, setIsNewTransactionModal] =
-        useState(false);
+  const [isNewTransactionModalOpen, setIsNewTransactionModal] = useState(false);
 
-    function handleOpenNewTransactionModal() {
-        setIsNewTransactionModal(true);
-    }
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModal(true);
+  }
 
-    function handleCloseNewTransactionModal() {
-        setIsNewTransactionModal(false);
-    }
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModal(false);
+  }
 
-    return (
-        <>
-            <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
-            <Dashboard />
-            <NewTransactionModal
-                isOpen={isNewTransactionModalOpen}
-                onRequestClose={handleCloseNewTransactionModal}
-            />
-            <GlobalStyle />
-        </>
-    );
+  return (
+    <TransactionProvider>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <Dashboard />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+      <GlobalStyle />
+    </TransactionProvider>
+  );
 }
